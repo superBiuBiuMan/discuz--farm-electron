ccc9507bd66916775ecdd90dc5c94cb3a884f67d
superBiuBiu <zmqdream@qq.com>
2024-04-10 10:50:17 +0800

    :sparkles: 添加基本icon

--- src/modules/farm/Operations/index.vue
+++ src/modules/farm/Operations/index.vue
@@ -1,18 +1,47 @@
 <script setup lang="ts">
-
+import LoginImg from "@/assets/favicon.png"
+import BagImg from "@/assets/btnIcons/bag.png";
+import WarehouseImg from "@/assets/btnIcons/warehouse.png";
+import StoreImg from "@/assets/btnIcons/store.png"
+import Setting from "@/assets/btnIcons/setting.png";
 </script>
 
 <template>
   <div class="operation">
     <a-space>
-      <a-button>背包</a-button>
-      <a-button>仓库</a-button>
-      <a-button>商店</a-button>
-      <a-button>设置</a-button>
+      <a-button class="operation_btns">
+        <template #icon><img :src="LoginImg" alt=""/></template>
+        <span class="operation_btns_text">登录</span>
+      </a-button>
+      <a-button class="operation_btns">
+        <template #icon><img :src="BagImg" alt=""/></template>
+        <span class="operation_btns_text">背包</span>
+      </a-button>
+      <a-button class="operation_btns">
+        <template #icon><img :src="WarehouseImg" alt=""/></template>
+        <span class="operation_btns_text">仓库</span>
+      </a-button>
+      <a-button class="operation_btns">
+        <template #icon><img :src="StoreImg" alt=""/></template>
+        <span class="operation_btns_text">商店</span>
+      </a-button>
+      <a-button class="operation_btns">
+        <template #icon><img :src="Setting" alt=""/></template>
+        <span class="operation_btns_text">设置</span>
+      </a-button>
     </a-space>
   </div>
 </template>
 
 <style scoped lang="sass">
-
+.operation
+  &_btns
+    display: flex
+    align-items: center
+    height: 50px
+    img
+      width: 40px
+      aspect-ratio:  1 /1
+    &_text
+      padding-left: 4px
 </style>
